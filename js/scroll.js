11d99533db3ca8be8117c452f3100b9218815de6
ommdl <148840048+ommdl@users.noreply.github.com>
2023-10-30 13:59:04 +0900

    誤字、リンク、車修正

--- js/scroll.js
+++ js/scroll.js
@@ -8,7 +8,7 @@ const list_wrap = document.querySelector('.scroll_wrap');
 // 全てのscroll_boxの幅
 
 gsap.to(list_wrap, {
-  x: () => -(list_wrap.clientWidth -  container.clientWidth),
+  x: () => -(list_wrap.clientWidth - container.clientWidth),
   // 横スクロール（左方向へ移動すること）なので、xプロパティにはマイナスを指定
   //scroll_wrapの横幅 – 親要素のscroll_containerの横幅
   //最後のscroll_boxがすべて見えるまでスクロールさせる
@@ -16,7 +16,6 @@ gsap.to(list_wrap, {
   scrollTrigger: {
     trigger: '.scroll',
     start: 'top top',
-
     end: () => (list_wrap.clientWidth - container.clientWidth),
     // レスポンシブ　幅サイズが変わっても対応してくれる
     scrub: true,
@@ -31,17 +30,14 @@ gsap.to(list_wrap, {
 
 const car = document.querySelector('.car_img');
 
-gsap.to('.car_img',{
-    // x: () => (window.innerWidth),
-    xPercent: 100,
-    scrollTrigger:{
-      trigger: 'car',
-      start: 'bottom bottom',
-        
-      //markers:true,
-      duration: 2,
-      delay: 3, //アニメーションの初期遅延（アニメーションが開始されるまでの指定）
-      duration: 50, //アニメーションの時間の設定
-      opacity: 1, //表示状態の指定
-toggleActions:'play none none reverse',    }
-  });
+gsap.to('.car_img', {
+  xPercent: 100,
+  duration: 3,
+
+  scrollTrigger: {
+    trigger: 'car',
+    start: 'bottom bottom',
+    // markers:true,
+    toggleActions: 'play none none reverse',
+  }
+});
