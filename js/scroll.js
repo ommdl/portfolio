530e5ac30f0bd8bb43bee555a6351f583858a173
ommdl <148840048+ommdl@users.noreply.github.com>
2023-11-18 02:31:26 +0900

    アップロード版に差し替え

--- js/scroll.js
+++ js/scroll.js
@@ -1,6 +1,5 @@
 'use strict';
 
-
 // GSAPで強制横スクロール
 const container = document.querySelector('.scroll_container');
 //全体の長さ
@@ -12,11 +11,11 @@ gsap.to(list_wrap, {
   // 横スクロール（左方向へ移動すること）なので、xプロパティにはマイナスを指定
   //scroll_wrapの横幅 – 親要素のscroll_containerの横幅
   //最後のscroll_boxがすべて見えるまでスクロールさせる
-  ease: 'none',
-  scrollTrigger: {
-    trigger: '.scroll',
+    ease: 'none',
+    scrollTrigger: {
+    trigger: '.bg_yellow',
     start: 'top top',
-    end: () => (list_wrap.clientWidth - container.clientWidth),
+  end: () => (list_wrap.clientWidth - container.clientWidth),
     // レスポンシブ　幅サイズが変わっても対応してくれる
     scrub: true,
     //スクロールとアニメーションを同期する。
@@ -35,9 +34,11 @@ gsap.to('.car_img', {
   duration: 3,
   delay:0.5,
   scrollTrigger: {
-    trigger: '.car_img',
+    trigger: '.bg_yellow',
     start: 'bottom bottom',
     // markers:true,
     toggleActions: 'play none none reverse',
   }
 });
+
+
